ca8da8b71648c1142020cfb9c81b61b124ad7471
Kleimionov-WebRuby <Kleimionov-WebRuby@yandex.by>
2019-11-24 10:12:06 +0300

    Added nodel for rating

---
+++ models/rating.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const { sequelize } = require('../database');
+
+const Rating = sequelize.define('rating', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+    unique: true,
+  },
+  ratingValue: {
+    type: Sequelize.INTEGER,
+    field: 'rating_value',
+    allowNull: false,
+  },
+});
+
+module.exports = Rating;
