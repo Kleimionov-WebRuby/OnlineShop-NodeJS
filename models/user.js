4144a81863c64873938efc4710f6f1938fd398a6
Kleimionov-WebRuby <Kleimionov-WebRuby@yandex.by>
2019-11-24 09:57:06 +0300

    Added model for user

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize');
+const { sequelize } = require('../database');
+
+const User = sequelize.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+    unique: true,
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    min: 0,
+    max: 30,
+    notEmpty: true,
+    field: 'first_name',
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    min: 0,
+    max: 30,
+    notEmpty: true,
+    field: 'last_name',
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    isEmail: true,
+    notEmpty: true,
+    unique: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true,
+  },
+  removeRequest: {
+    type: Sequelize.BOOLEAN,
+    field: 'remove_request',
+    defaultValue: 0,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
