b62ae1b3be8b797b62eeef32979ad3c208899add
Kleimionov-WebRuby <Kleimionov-WebRuby@yandex.ru>
2020-01-09 10:05:52 +0300

    Added different environment for production and development

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -1,9 +1,17 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  development: {
+  production: {
     username: process.env.MYSQL_DB_USER,
     password: process.env.MYSQL_DB_PASSWORD,
     database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_DB_HOST || 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.MYSQL_DB_HOST || 'localhost',
     dialect: 'mysql',
     operatorsAliases: false,
